35ace6bc809e61bde53e2dbe05834e0b63023f7d
Atul Sahay <atulsahay01@gmail.com>
2020-10-07 21:10:33 +0530

    removed header

--- train_multitask.py
+++ train_multitask.py
@@ -1,8 +1,3 @@
-# @Author: Atul Sahay <atul>
-# @Date:   2020-06-09T13:57:57+05:30
-# @Email:  atulsahay01@gmail.com
-# @Last modified by:   atul
-# @Last modified time: 2020-06-09T17:00:34+05:30
 
 
 
